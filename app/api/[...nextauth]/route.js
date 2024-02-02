a3b0ea1800dbca556d6498da940bb7c53b2007f5
yanyu <yanyu_chen@fis.edu>
2024-02-02 22:42:36 +0100

    34567890zgvghhj

--- app/api/[...nextauth]/route.js
+++ app/api/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import PostgresAdapter from "../../../lib/adapter";
+import PostgresAdapter from "@auth/pg-adapter";
 
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
