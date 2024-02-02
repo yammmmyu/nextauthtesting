fdc6828f992069d4d436bd75954d7d33083d55ca
yanyu <yanyu_chen@fis.edu>
2024-02-02 22:54:19 +0100

    zfgvhbjnkhfcgvhbjnksfdcxysdafdwq

--- app/api/[...nextauth]/route.js
+++ app/api/[...nextauth]/route.js
@@ -1,17 +1,20 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import PostgresAdapter from "@auth/pg-adapter";
+import { Pool } from 'pg';
 
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
-export default NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  adapter: PostgresAdapter(pool),
-});
+export default function authHandler(req, res) {
+  return NextAuth(req, res, {
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      }),
+    ],
+    adapter: PostgresAdapter(pool),
+  });
+}
