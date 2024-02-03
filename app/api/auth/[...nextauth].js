3446e1f2ab915e2266fdd6071ff45c520a3255de
yanyu <yanyu_chen@fis.edu>
2024-02-03 11:09:58 +0100

    env files

--- app/api/auth/[...nextauth].js
+++ app/api/auth/[...nextauth].js
@@ -4,14 +4,14 @@ import PostgresAdapter from "@auth/pg-adapter";
 import { Pool } from 'pg';
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   adapter: PostgresAdapter(pool),
